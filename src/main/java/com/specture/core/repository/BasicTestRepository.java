7c1ddc357cf9ff0ac69f13ea2308fa405a056ccc
Yurii Chukhlatyi <ychukhlatyi@evenz.co>
2020-12-14 12:31:41 +0200

    Build fixes Synchronized spring boot version Fixed repository creation

--- src/main/java/com/specture/core/repository/BasicTestRepository.java
+++ src/main/java/com/specture/core/repository/BasicTestRepository.java
@@ -4,6 +4,7 @@ import com.specture.core.model.BasicTest;
 import com.specture.core.model.EnrichedPageWithAggregations;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Timestamp;
@@ -11,12 +12,10 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface BasicTestRepository {
+public interface BasicTestRepository extends ElasticsearchRepository<BasicTest, String> {
     Page<BasicTest> findByClientUuid(String clientUuid, Pageable pageable );
 
     Page<BasicTest> findAll(Pageable pageable);
-
-    String save(BasicTest basicTest);
 
     Page<BasicTest> findByTypeOfProbePort(String typeofProbe, Pageable page);
     EnrichedPageWithAggregations<BasicTest> findByTypeOfProbePortPackagesAndDate(String typeofProbe, List<Long> packages, List<String> probes, LocalDateTime from, LocalDateTime to, Pageable page);

6c055401131eafd48f3e34e754ab83e4fce69020
Yurii Chukhlatyi <ychukhlatyi@evenz.co>
2020-12-17 15:57:26 +0200

    Refactor constants

--- src/main/java/com/specture/core/constant/PropertyName.java
+++
@@ -1,9 +0,0 @@
-package com.specture.core.constant;
-
-public interface PropertyName {
-    String ADVERTISED_NAME = "advertisedName";
-    String UPLOAD_METRIC_PARAMETER = "Upload";
-    String DOWNLOAD_METRIC_PARAMETER = "Download";
-    String PING_METRIC_PARAMETER = "Latency";
-    String JITTER_METRIC_PARAMETER = "Jitter";
-}

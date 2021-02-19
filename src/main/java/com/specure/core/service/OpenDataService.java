a62462b9d598430b99c7a269c2e4ae8ce806497b
Anton Batalin <antbat2009@gmail.com>
2021-02-19 13:54:21 +0200

    list separator as parameter

--- src/main/java/com/specure/core/service/OpenDataService.java
+++ src/main/java/com/specure/core/service/OpenDataService.java
@@ -4,6 +4,6 @@ import com.specure.core.enums.DigitalSeparator;
 import org.springframework.http.ResponseEntity;
 
 public interface OpenDataService {
-    ResponseEntity<Object> getOpenDataMonthlyExport(Integer year, Integer month, String fileExtension, DigitalSeparator digitalSeparator);
-    ResponseEntity<Object> getOpenDataFullExport(String fileExtension, DigitalSeparator digitalSeparator);
+    ResponseEntity<Object> getOpenDataMonthlyExport(Integer year, Integer month, String fileExtension, DigitalSeparator digitalSeparator, char listSeparator);
+    ResponseEntity<Object> getOpenDataFullExport(String fileExtension, DigitalSeparator digitalSeparator, char listSeparator);
 }
